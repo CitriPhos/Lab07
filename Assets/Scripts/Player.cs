4b657f6e56b43e895a1cb11cd28c605fdee1e2d5
CitriPhos <jan.nathanielle_strachan@yahoo.com>
2021-10-19 15:53:08 +0800

    Implemented features required for Unity project
    - Implemented features where:
      - duck will "fly" upwards when spacebar is pressed
      - ensure player doesn't go above the screen
      - game over when player falls below screen or collides with obstacle

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float jumpForce;
+    private Rigidbody rb;
+
     private Animation thisAnimation;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
     }
 
     void Update()
     {
+        Vector3 pos = transform.position;
+        
+        if (transform.position.y > 4)
+        {
+            pos.y = 4;
+            transform.position = pos;
+            rb.velocity = Vector3.zero;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             thisAnimation.Play();
+        }
+
+        if (transform.position.y < -4)
+        {
+            GameManager.thisManager.GameOver();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Obstacle"))
+        {
+            GameManager.thisManager.GameOver();
+        }
     }
 }
